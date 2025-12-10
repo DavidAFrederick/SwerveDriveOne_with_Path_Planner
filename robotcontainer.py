321ed8905b0f91d12989727730b32b16be53ad3e
David Frederick <davidafrederick.va.usa.earth@gmail.com>
2025-12-09 19:38:39 -0500

    Added a Dummy Vision Subsystem to reduce errors in log

--- robotcontainer.py
+++ robotcontainer.py
@@ -98,9 +98,14 @@ class RobotContainer:
 
         self._apriltag_alignment_data = AprilTagAlignmentData()
         self._ledsubsystem = LEDSubsystem()
-        # self._visionsubsystem = VisionSystem(self._apriltag_alignment_data)   #  Commented out 
+
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+        #  TEMPORARY - Commented out until ready to test
+        # Putting in a temporary placehold to reduce the number of error messages in Console
+
         self._visionsubsystem = VisionSystemDUMMY()
-        
+        # self._visionsubsystem = VisionSystem(self._apriltag_alignment_data)   
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
         self._ledsubsystem.setDefaultCommand(LEDCommand( self._ledsubsystem, 100))
 
